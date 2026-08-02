a60ebe36e0d60c50363788395efe6b252b2666ec
Ratish149 <ratish.shakya149@gmail.com>
2026-08-02 16:14:24 +0545

    refactor: integrate Unfold ModelAdmin into Announcement and EmergencyAlert admin configurations

--- announcement/admin.py
+++ announcement/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import Announcement
 
 
 @admin.register(Announcement)
-class AnnouncementAdmin(admin.ModelAdmin):
+class AnnouncementAdmin(ModelAdmin):
     list_display = ("title", "slug", "link", "created_at", "updated_at")
     search_fields = ("title", "slug", "link")
     list_filter = ("created_at", "updated_at")
--- emergency_alert/admin.py
+++ emergency_alert/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import EmergencyAlert
 
 
 @admin.register(EmergencyAlert)
-class EmergencyAlertAdmin(admin.ModelAdmin):
+class EmergencyAlertAdmin(ModelAdmin):
     list_display = ("title", "slug", "link", "created_at", "updated_at")
     search_fields = ("title", "slug", "link")
     list_filter = ("created_at", "updated_at")
